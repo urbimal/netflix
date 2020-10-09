df67cbbad68dbbaaceed58d848ab915779ee5902
urbimal <50596418+urbimal@users.noreply.github.com>
2020-10-09 20:09:57 +0300

    "trebuie sa plec" commit

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyBnDBdg-zeIT7yul_W5QlXnu0Gxo7yGb8w',
+  authDomain: 'netflix-a13b7.firebaseapp.com',
+  databaseURL: 'https://netflix-a13b7.firebaseio.com',
+  projectId: 'netflix-a13b7',
+  storageBucket: 'netflix-a13b7.appspot.com',
+  messagingSenderId: '718977122691',
+  appId: '1:718977122691:web:7298ca4bc600ef6d9ab6b6',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,67 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import HeaderContainer from '../containers/header';
+import FooterContainer from '../containers/footer';
+import Form from '../components/form';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
-  return <p>signin</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+  const handleSignIn = (event) => {
+    event.PreventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign in</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autocomplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign in
+            </Form.Submit>
+            <Form.Text>
+              New to Netflix? <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCaptcha to ensure you are not a bot.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
